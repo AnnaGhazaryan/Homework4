c23fdad731f54301120d5025d61e49e5f6ca6836
AnnaGhazaryan <58561909+AnnaGhazaryan@users.noreply.github.com>
2019-12-25 23:40:50 +0400

    Update Exercise2.js

--- Exercise2.js
+++ Exercise2.js
@@ -1,4 +1,4 @@
-var x = 5;
+var x = -5;
 var y = 22;
 
 function new_array(a,b){
@@ -9,7 +9,7 @@ function new_array(a,b){
 
 while (a<=b){
   odd = 0;
-  for (k = a; Math.floor(k) !==0; k =Math.floor(k / 10)){
+  for (k = Math.abs(a); Math.floor(k) !==0; k =Math.floor(k / 10)){
     i = k % 10;
     if(i % 2 != 0){
       odd += 1;
@@ -24,3 +24,4 @@ while (a<=b){
 return(array);
 }
 new_array(x, y);
+

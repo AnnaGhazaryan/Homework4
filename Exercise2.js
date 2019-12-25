5ecb6647723089d8ab1c44ce62fabdb221cfa32f
AnnaGhazaryan <58561909+AnnaGhazaryan@users.noreply.github.com>
2019-12-25 23:38:34 +0400

    Create Exercise2.js

---
+++ Exercise2.js
@@ -0,0 +1,26 @@
+var x = 5;
+var y = 22;
+
+function new_array(a,b){
+  var odd;
+  var array = [];
+  var i;
+  var k;
+
+while (a<=b){
+  odd = 0;
+  for (k = a; Math.floor(k) !==0; k =Math.floor(k / 10)){
+    i = k % 10;
+    if(i % 2 != 0){
+      odd += 1;
+    }
+  }
+  if (odd === 0) {
+    array.push(a);
+  }
+  a ++;
+}
+ console.log(array);
+return(array);
+}
+new_array(x, y);
